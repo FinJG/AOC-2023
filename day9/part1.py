03d7ee420a0d466d7691555ecdbf41bdb497698a
FinJG <finlay.griffiths03@gmail.com>
2023-12-15 12:48:13 +0000

    update day9

--- day9/part1.py
+++ day9/part1.py
@@ -5,7 +5,6 @@ values = [0] * len(file)
 index = 0
 for i in file:
     sequence = [int(i) for i in i.split()]   
-
     while any(sequence):
         values[index] += sequence[-1]
         sequence = [sequence[j + 1] - sequence[j] for j in range(len(sequence) - 1)]
--- day9/part2.py
+++ day9/part2.py
@@ -4,12 +4,10 @@ with open("input.txt", "r") as f:
 values = [0] * len(file)
 index = 0
 for i in file:
-    sequence = [int(i) for i in reversed(i.split())]   
-
+    sequence = [int(i) for i in i.split()]   
     while any(sequence):
-        values[index] += sequence[-1]
-        sequence = [sequence[j + 1] - sequence[j] for j in range(len(sequence) - 1)]
+        values[index] += sequence[0]
+        sequence = [sequence[j] - sequence[j + 1] for j in range(len(sequence) - 1)]
     index += 1   
-            
-print(sum(values))
 
+print(sum(values))
